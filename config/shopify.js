4f1ddb091741ec7845cfec560bd85990ff2b111b
Taher Khamaysa <taherkhamaysa@gmail.com>
2025-01-16 22:29:30 +0200

    v 1.3

--- config/shopify.js
+++ config/shopify.js
@@ -1,13 +1,13 @@
-import { Shopify, ApiVersion } from "@shopify/shopify-api";
+import { Shopify } from "@shopify/shopify-api";
 
 export const initializeShopifyContext = () => {
   Shopify.Context.initialize({
     API_KEY: process.env.SHOPIFY_API_KEY,
     API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
     SCOPES: ["write_customers"],
-    HOST_NAME: process.env.SHOPIFY_STORE_URL.replace(/https?:\/\//, ''),
-    IS_EMBEDDED_APP: false,
-    API_VERSION: ApiVersion.January24,
+    HOST_NAME: process.env.SHOPIFY_STORE_URL.replace(/https?:\/\//, ""),
+    IS_EMBEDDED_APP: true,
+    API_VERSION: "2023-07",
   });
 };
 
