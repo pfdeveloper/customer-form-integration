8a6be57435df8e6e218feb20270a6887a35779b5
Taher Khamaysa <taherkhamaysa@gmail.com>
2025-01-16 11:29:08 +0200

    5th

--- api/create-customer.js
+++ api/create-customer.js
@@ -3,17 +3,12 @@
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://development-km.myshopify.com"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
 
-  // Handle preflight requests (OPTIONS)
-  if (req.method === "OPTIONS") {
-    return res.status(200).end(); // Respond to preflight request with 200
-  }
+
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   // Only allow POST requests
   if (req.method !== "POST") {
