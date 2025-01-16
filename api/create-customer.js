4c7921bc70b42e65563c6d66851a08f70f24ecf2
Taher Khamaysa <taherkhamaysa@gmail.com>
2025-01-16 21:54:26 +0200

    v 1.2

--- api/create-customer.js
+++ api/create-customer.js
@@ -11,6 +11,10 @@ initializeShopifyContext();
  * @param {Object} res - HTTP response object.
  */
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); 
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed. Use POST." });
   }
