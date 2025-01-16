b3b88f26836906893c4269ece4fe77d0da2a885e
Taher Khamaysa <taherkhamaysa@gmail.com>
2025-01-16 23:44:18 +0200

    v 2.0

--- config/shopify.js
+++ config/shopify.js
@@ -1,11 +1,7 @@
-import dotenv from "dotenv";
 import { shopifyApi, LATEST_API_VERSION } from "@shopify/shopify-api";
 
-// Load environment variables from .env file
-dotenv.config();
-
 const shopify = shopifyApi({
-  apiKey: "36838ce9cc099738ae13c2dc9fef14dc",
+  apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET,
   scopes: ["write_customers"],
   hostName: process.env.SHOPIFY_STORE_URL.replace(/https?:\/\//, ""),
