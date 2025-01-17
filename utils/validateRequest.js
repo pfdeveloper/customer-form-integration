b5b814e47f343a84f3afb5ba61f9593be714e330
Taher Khamaysa <taherkhamaysa@gmail.com>
2025-01-17 03:45:20 +0200

    v 3.00

--- utils/validateRequest.js
+++
@@ -1,8 +0,0 @@
-export const validateCreateCustomerRequest = (body) => {
-  const { customer } = body;
-
-  if (!customer) return "Missing customer parameter";
-
-  // Additional customer validation can be added here if needed
-  return null; // No errors
-};
